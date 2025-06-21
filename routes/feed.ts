35b611c164ea5ec6ba26602ca09d93b4c25f6f19
Godwinss24 <godwinsudoinyang@gmail.com>
2025-06-21 08:40:12 +0100

    added-more-categories

--- routes/feed.ts
+++ routes/feed.ts
@@ -1,13 +1,100 @@
 import express, { Response, Router } from "express";
 import { Parser } from "xml2js";
 import { RSSController } from "../controller/rss/rss.controller";
-import {  SportRSS, TechRSS } from "../services/rss.service";
+import {
+  BaseRSS,
+  TechRSS,
+  SportRSS,
+  WorldRSS,
+  BusinessRSS,
+  PoliticsRSS,
+  HealthRSS,
+  ScienceRSS,
+  EntertainmentRSS,
+  UKRSS,
+  EnglandRSS,
+  WalesRSS,
+  ScotlandRSS,
+  NorthernIrelandRSS,
+  MagazineRSS,
+  EducationRSS,
+  InPicturesRSS,
+  VideoAndAudioRSS,
+  NewsbeatRSS,
+  StoriesRSS,
+  HaveYourSayRSS,
+  WorldAfricaRSS,
+  WorldAsiaRSS,
+  WorldEuropeRSS,
+  WorldLatinAmericaRSS,
+  WorldMiddleEastRSS,
+  WorldUsCanadaRSS,
+  ClimateRSS,
+  CoronavirusRSS
+} from "../services/rss.service";
 
 const router = express.Router();
+
 const techService = new TechRSS();
 const sportService = new SportRSS();
+const worldService = new WorldRSS();
+const businessService = new BusinessRSS();
+const politicsService = new PoliticsRSS();
+const healthService = new HealthRSS();
+const scienceService = new ScienceRSS();
+const entertainmentService = new EntertainmentRSS();
+const ukService = new UKRSS();
+const englandService = new EnglandRSS();
+const walesService = new WalesRSS();
+const scotlandService = new ScotlandRSS();
+const northernIrelandService = new NorthernIrelandRSS();
+const magazineService = new MagazineRSS();
+const educationService = new EducationRSS();
+const inPicturesService = new InPicturesRSS();
+const videoAndAudioService = new VideoAndAudioRSS();
+const newsbeatService = new NewsbeatRSS();
+const storiesService = new StoriesRSS();
+const haveYourSayService = new HaveYourSayRSS();
+const worldAfricaService = new WorldAfricaRSS();
+const worldAsiaService = new WorldAsiaRSS();
+const worldEuropeService = new WorldEuropeRSS();
+const worldLatinAmericaService = new WorldLatinAmericaRSS();
+const worldMiddleEastService = new WorldMiddleEastRSS();
+const worldUsCanadaService = new WorldUsCanadaRSS();
+const climateService = new ClimateRSS();
+const coronavirusService = new CoronavirusRSS();
 
-const rssController = new RSSController(techService, sportService);
+const rssController = new RSSController(
+  techService,
+  sportService,
+  worldService,
+  businessService,
+  politicsService,
+  healthService,
+  scienceService,
+  entertainmentService,
+  ukService,
+  englandService,
+  walesService,
+  scotlandService,
+  northernIrelandService,
+  magazineService,
+  educationService,
+  inPicturesService,
+  videoAndAudioService,
+  newsbeatService,
+  storiesService,
+  haveYourSayService,
+  worldAfricaService,
+  worldAsiaService,
+  worldEuropeService,
+  worldLatinAmericaService,
+  worldMiddleEastService,
+  worldUsCanadaService,
+  climateService,
+  coronavirusService
+);
+
 
 
 router.get("/", async (req, res) => {
--- services/rss.service.ts
+++ services/rss.service.ts
@@ -39,15 +39,170 @@ export class BaseRSS {
 
 }
 
-
 export class TechRSS extends BaseRSS {
-    constructor(){
-        super(RSSlinks.TECH)
+    constructor() {
+        super(RSSlinks.TECH);
     }
 }
 
 export class SportRSS extends BaseRSS {
-    constructor(){
-        super(RSSlinks.SPORT)
+    constructor() {
+        super(RSSlinks.SPORT);
+    }
+}
+
+export class WorldRSS extends BaseRSS {
+    constructor() {
+        super(RSSlinks.WORLD);
+    }
+}
+
+export class BusinessRSS extends BaseRSS {
+    constructor() {
+        super(RSSlinks.BUSINESS);
+    }
+}
+
+export class PoliticsRSS extends BaseRSS {
+    constructor() {
+        super(RSSlinks.POLITICS);
+    }
+}
+
+export class HealthRSS extends BaseRSS {
+    constructor() {
+        super(RSSlinks.HEALTH);
+    }
+}
+
+export class ScienceRSS extends BaseRSS {
+    constructor() {
+        super(RSSlinks.SCIENCE);
+    }
+}
+
+export class EntertainmentRSS extends BaseRSS {
+    constructor() {
+        super(RSSlinks.ENTERTAINMENT);
+    }
+}
+
+export class UKRSS extends BaseRSS {
+    constructor() {
+        super(RSSlinks.UK);
+    }
+}
+
+export class EnglandRSS extends BaseRSS {
+    constructor() {
+        super(RSSlinks.ENGLAND);
+    }
+}
+
+export class WalesRSS extends BaseRSS {
+    constructor() {
+        super(RSSlinks.WALES);
+    }
+}
+
+export class ScotlandRSS extends BaseRSS {
+    constructor() {
+        super(RSSlinks.SCOTLAND);
+    }
+}
+
+export class NorthernIrelandRSS extends BaseRSS {
+    constructor() {
+        super(RSSlinks.NORTHERN_IRELAND);
+    }
+}
+
+export class MagazineRSS extends BaseRSS {
+    constructor() {
+        super(RSSlinks.MAGAZINE);
+    }
+}
+
+export class EducationRSS extends BaseRSS {
+    constructor() {
+        super(RSSlinks.EDUCATION);
+    }
+}
+
+export class InPicturesRSS extends BaseRSS {
+    constructor() {
+        super(RSSlinks.IN_PICTURES);
+    }
+}
+
+export class VideoAndAudioRSS extends BaseRSS {
+    constructor() {
+        super(RSSlinks.VIDEO_AND_AUDIO);
+    }
+}
+
+export class NewsbeatRSS extends BaseRSS {
+    constructor() {
+        super(RSSlinks.NEWSBEAT);
+    }
+}
+
+export class StoriesRSS extends BaseRSS {
+    constructor() {
+        super(RSSlinks.STORIES);
+    }
+}
+
+export class HaveYourSayRSS extends BaseRSS {
+    constructor() {
+        super(RSSlinks.HAVE_YOUR_SAY);
+    }
+}
+
+export class WorldAfricaRSS extends BaseRSS {
+    constructor() {
+        super(RSSlinks.WORLD_AFRICA);
+    }
+}
+
+export class WorldAsiaRSS extends BaseRSS {
+    constructor() {
+        super(RSSlinks.WORLD_ASIA);
+    }
+}
+
+export class WorldEuropeRSS extends BaseRSS {
+    constructor() {
+        super(RSSlinks.WORLD_EUROPE);
+    }
+}
+
+export class WorldLatinAmericaRSS extends BaseRSS {
+    constructor() {
+        super(RSSlinks.WORLD_LATIN_AMERICA);
+    }
+}
+
+export class WorldMiddleEastRSS extends BaseRSS {
+    constructor() {
+        super(RSSlinks.WORLD_MIDDLE_EAST);
+    }
+}
+
+export class WorldUsCanadaRSS extends BaseRSS {
+    constructor() {
+        super(RSSlinks.WORLD_US_CANADA);
+    }
+}
+
+export class ClimateRSS extends BaseRSS {
+    constructor() {
+        super(RSSlinks.CLIMATE);
+    }
+}
+
+export class CoronavirusRSS extends BaseRSS {
+    constructor() {
+        super(RSSlinks.CORONAVIRUS);
     }
 }
